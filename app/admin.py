033078b094dbc8809304a0d469f7f38fa6a46c14
Aristofany Herderson <programmeraristofanyherderson@gmail.com>
2024-12-05 12:04:35 -0300

    :passport_control: permission: add logged user permission to view Players database, change logout redirect url

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Player
 
-# Register your models here.
+class PlayerModel(admin.ModelAdmin):
+  list_display = ["username", "flips_quantity", "used_time", "has_completed"]
+  list_filter = ["flips_quantity"]
+  search_fields = ["username"]
+
+admin.site.register(Player, PlayerModel)
